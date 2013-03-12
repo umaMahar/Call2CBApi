42d4e47b12e3196199128f86ff58a484b7000a3b
Uma Maharajan <uma.maharajan@careerbuilder.com>
2013-03-12 17:05:22 -0400

    updating the latest changes

---
+++ app/models/apicalltocb.rb
@@ -0,0 +1,26 @@
+require 'HTTParty'
+require 'uri'
+require 'json'
+
+
+class Apicalltocb
+	include HTTParty
+
+	def self.getJob(did,dev_key)	
+  	resp = get('http://api.careerbuilder.com/v1/job', :query => {:developerkey => dev_key, :did => did})
+    json_hash = resp.parsed_response
+    return json_hash   
+	end
+
+	def self.getByKeyword(kywd,loc,dev_key)		
+  	resp = get('http://api.careerbuilder.com/v1/jobsearch', :query => {:developerkey => dev_key, :Keywords => kywd, :Location => loc})
+  	json_hash = resp.parsed_response        
+    return json_hash
+	end
+
+end
+
+
+
+
+
